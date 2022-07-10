f9306e4072b7e8b5bb3de5ab1d2a78e873ea0973
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2022-07-10 11:12:03 -0500

    Add initial Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "terraform"
+
+  default_tags {
+    tags = {
+      project = "my-carbon-impact"
+    }
+  }
+}
